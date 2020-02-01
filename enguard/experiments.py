e67f5e6695968a1ae8045cece2d03a840493b4a6
James <james@thefreshuk.com>
2020-02-01 22:08:53 +0000

    feat: add utility to create temporary git repo

--- enguard/experiments.py
+++ enguard/experiments.py
@@ -1,3 +1,14 @@
 """Experiment with potential solutions."""
 
+import tempfile
+import git
+from pydriller import GitRepository
+
 ASSERTION = True
+
+
+def init_temp_repo():
+    """Init a temporary git repo."""
+    dir = tempfile.TemporaryDirectory()
+    git.Repo.init(dir.name)
+    return GitRepository(dir.name), dir
--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -1,8 +1,25 @@
 """Test output of experiments for features under development."""
 
+from typing import Tuple
+import pytest
+import git
+from pydriller import GitRepository
+from tempfile import NamedTemporaryFile
 from enguard import experiments
 
 
-def test_experiment_tests():
+@pytest.fixture
+def git_repo():
+    """Set up a new git repo."""
+    repo, dir = experiments.init_temp_repo()
+    yield repo, dir.name
+    dir.cleanup()
+
+
+def test_experiment_tests(git_repo: Tuple[GitRepository, str]):
     """Test setup for experiments."""
-    assert experiments.ASSERTIONS
+    repo, dirname = git_repo
+
+    with NamedTemporaryFile(dir=dirname, delete=False) as file:
+        repo.repo.index.add([file.name])
+        assert file.name in repo.files()
