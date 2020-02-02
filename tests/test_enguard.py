04e34ee9b98d54e495674a6f95c9d15a89f6a0af
James <james@thefreshuk.com>
2020-02-02 08:55:42 +0000

    test: add stubs for init acceptance tests

--- tests/test_enguard.py
+++ tests/test_enguard.py
@@ -16,3 +16,13 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_init_creates_a_default_config_file():
+    """Test init command creates the default config file."""
+    pass
+
+
+def test_init_installs_git_hooks():
+    """Test init command installs a script for all git hooks."""
+    pass
